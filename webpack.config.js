b47e6c702d4e8f195e9ebb28f13dc99ab0873436
leeinho <leeinho@soleo.kr>
2022-01-24 16:42:39 +0900

    devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,56 @@
 var path = require('path');
+var MinCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'none',
 
-    entry: './src/app.js',
+    entry: {
+        app: './src/app.js',
+        index: './code-spliting/index.js'
+    },
 
     output: {
-        filename: 'main.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
 
     module:{
+        rules: [            
+            { 
+                test: /\.css$/,
+                use: [
+                    {loader: MinCssExtractPlugin.loader},
+                    {loader: 'css-loader'}
+                ]
+            }
+            /*
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }, 
 
+            {
+                test: /\.sass$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+            
+            {
+                test: /\.js$/,
+                use: ['babel-loader']
+            }*/
+        ]
     },
     
+    plugins : [ 
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            // 템플릿 기반으로 빌드 결과물을 추가해줌
+            template: './code-spliting/index.html'
+        })
+    ],
+
     stats:{
         colors: true
     },
